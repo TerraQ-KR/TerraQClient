3cd7a9de48fc4032319cc48975098929b22b4c26
hann-bae <bhn4542@gmail.com>
2023-02-26 00:07:53 +0900

    init:questlist

---
+++ lib/screens/quest/quest_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quest List'),
+      ),
+      body: const Center(
+        child: Text('Quest List'),
+      ),
+    );
+  }
+}
