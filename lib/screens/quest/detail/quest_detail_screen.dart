2bf015dbf4883e4644acddb620a38f1439ad3982
hann-bae <bhn4542@gmail.com>
2023-02-27 16:50:52 +0900

    init: quest_detail_sceen

--- lib/screens/quest/detail/quest_detail_screen.dart
+++ lib/screens/quest/detail/quest_detail_screen.dart
@@ -1,14 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:eco_reward_app/utils/color_utils.dart';
+import 'package:eco_reward_app/screens/quest/main/widget/button_quest_common.dart';
 
 class QuestDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Quest Detail'),
-      ),
-      body: const Center(
-        child: Text('Quest Detail'),
+      appBar: AppBar(title: const Text('Quest Detail')),
+      body: Container(
+        width: 390,
+        height: 844,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [ColorUtils.subOrange, ColorUtils.primary],
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: SizedBox(
+                  width: 360,
+                  height: 488,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 360,
+                        height: 488,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                            bottomLeft: Radius.circular(0),
+                            bottomRight: Radius.circular(0),
+                          ),
+                          color: ColorUtils.white,
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 42,
+                          right: 40,
+                          top: 37,
+                          bottom: 131,
+                        ),
+                        child: const Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 100,
+              child: ButtonQuest(),
+            ),
+            const Positioned(
+              left: 16,
+              top: 168,
+              child: Text(
+                " Quest name",
+                style: TextStyle(
+                  color: ColorUtils.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/quest/detail/quest_status_screen.dart
+++ lib/screens/quest/detail/quest_status_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:eco_reward_app/utils/color_utils.dart';
 
 class QuestStatusScreen extends StatelessWidget {
   @override
@@ -7,8 +8,30 @@ class QuestStatusScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Quest Status'),
       ),
-      body: const Center(
-        child: Text('Quest Status'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [ColorUtils.subOrange, ColorUtils.primary]),
+        ),
+        child: Positioned(
+          bottom: 0,
+          child: Center(
+            child: Container(
+              width: 360,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                ),
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
