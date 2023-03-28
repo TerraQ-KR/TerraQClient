0dff5cda97aa71b4ba8d8aa46e579282826755ed
hann-bae <bhn4542@gmail.com>
2023-03-28 12:40:39 +0900

    feat(fix) : quest null error handling

--- lib/screens/quest/main/models/get_myquest.dart
+++ lib/screens/quest/main/models/get_myquest.dart
@@ -5,22 +5,23 @@ List<TMyQuest> getMyQuest(dataSource) {
     return [];
   }
 
-  return (dataSource as List<dynamic>).map<TMyQuest>((dynamic item) {
-    return TMyQuest(
-      memDoId: item['memDoId'] ?? 0,
-      questName: item['questDto']['questName'] ?? 'questName',
-      questId: item['questDto']['questId'] ?? 0,
-      subCategoryName: item['questDto']['categoryDto']['subCategoryName'] ??
-          'subCategoryName',
-      reward: item['questDto']['reward'] ?? 0,
-      briefing: item['questDto']['briefing'] ?? 'null',
-      timeLimit: item['questDto']['timeLimit'] ?? 0,
-      challenger: item['questDto']['challenger'] ?? 0,
-      startDate: item['startDate'] ?? 'null',
-      dueDate: item['dueDate'] ?? 'null',
-      // leftTime: item['leftTime'] ?? 0,
-      // stance: item['stance'] ?? 0,
-      // achieveRate: item['achieveRate'] ?? 0,
-    );
-  }).toList();
+  return (dataSource as List<dynamic>?)?.map<TMyQuest>((dynamic item) {
+        return TMyQuest(
+          memDoId: item['memDoId'] ?? 0,
+          questName: item['questDto']['questName'] ?? 'questName',
+          questId: item['questDto']['questId'] ?? 0,
+          subCategoryName: item['questDto']['categoryDto']['subCategoryName'] ??
+              'subCategoryName',
+          reward: item['questDto']['reward'] ?? 0,
+          briefing: item['questDto']['briefing'] ?? 'null',
+          timeLimit: item['questDto']['timeLimit'] ?? 0,
+          challenger: item['questDto']['challenger'] ?? 0,
+          startDate: item['startDate'] ?? 'null',
+          dueDate: item['dueDate'] ?? 'null',
+          // leftTime: item['leftTime'] ?? 0,
+          // stance: item['stance'] ?? 0,
+          // achieveRate: item['achieveRate'] ?? 0,
+        );
+      }).toList() ??
+      [];
 }
--- lib/screens/quest/main/models/get_quest.dart
+++ lib/screens/quest/main/models/get_quest.dart
@@ -5,16 +5,17 @@ List<TQuest> getQuestNotMyQuestList(dataSource) {
     return [];
   }
 
-  return (dataSource as List<dynamic>).map<TQuest>((dynamic item) {
-    return TQuest(
-      id: item['id'] ?? 0,
-      questName: item['questName'] ?? '',
-      questId: item['questId'] ?? 0,
-      subCategroyName: item['categoryDto']['subCategoryName'] ?? 'sub',
-      reward: item['reward'] ?? 0,
-      briefing: item['briefing'] ?? 'null',
-      timeLimit: item['timeLimit'] ?? 0,
-      nowChallenger: item['nowChallenger'] ?? 0,
-    );
-  }).toList();
+  return (dataSource as List<dynamic>?)?.map<TQuest>((dynamic item) {
+        return TQuest(
+          id: item['id'] ?? 0,
+          questName: item['questName'] ?? '',
+          questId: item['questId'] ?? 0,
+          subCategroyName: item['categoryDto']['subCategoryName'] ?? 'sub',
+          reward: item['reward'] ?? 0,
+          briefing: item['briefing'] ?? 'null',
+          timeLimit: item['timeLimit'] ?? 0,
+          nowChallenger: item['nowChallenger'] ?? 0,
+        );
+      }).toList() ??
+      [];
 }
